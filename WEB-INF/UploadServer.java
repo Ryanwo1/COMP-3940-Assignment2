9a8b0dd9c612a145aa8e5ba02f448c48bf3a22be
Ryan Odribege <rodribege@my.bcit.ca>
2022-10-11 05:54:21 -0700

    verify thread starts

--- WEB-INF/UploadServer.java
+++ WEB-INF/UploadServer.java
@@ -2,26 +2,14 @@ import java.net.*;
 import java.io.*;
 public class UploadServer {
     public static void main(String[] args) throws IOException {
-
         ServerSocket serverSocket = null;
         final int portNumber = 4999;
-        String httpRequest = "GET / HTTP/1.1";
-        String hostHeader = "Host: ";
+
+
         try {
             serverSocket = new ServerSocket(portNumber);
-            Socket socket = serverSocket.accept();
-            PrintWriter toServerPrintWriter= new PrintWriter(socket.getOutputStream());
-            toServerPrintWriter.println(httpRequest);
-            toServerPrintWriter.println(hostHeader);
-            toServerPrintWriter.println();
-            toServerPrintWriter.flush();
+            // PrintWriter toServerPrintWriter= new PrintWriter(socket.getOutputStream());
 
-            BufferedReader fromServerBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String str;
-            while ((str = fromServerBufferedReader.readLine()) != null) {
-                System.out.println(str);
-            }
             System.out.println("Server up on port " + portNumber);
         } catch (IOException e) {
             System.err.println("Could not listen on port: " + portNumber + ".");
@@ -29,6 +17,8 @@ public class UploadServer {
         }
         while (true) {
             new UploadServerThread(serverSocket.accept()).start();
+            System.out.println("New client");
+
         }
     }
 }
