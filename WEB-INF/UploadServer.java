56fe302da358ae29d25037661bf360b8789378a2
Ryan Odribege <rodribege@my.bcit.ca>
2022-10-10 22:32:56 -0700

    Write frame of HttpRequest (at Transport level)

--- WEB-INF/UploadServer.java
+++ WEB-INF/UploadServer.java
@@ -5,9 +5,23 @@ public class UploadServer {
 
         ServerSocket serverSocket = null;
         final int portNumber = 4999;
+        String httpRequest = "GET / HTTP/1.1";
+        String hostHeader = "Host: ";
         try {
             serverSocket = new ServerSocket(portNumber);
             Socket socket = serverSocket.accept();
+            PrintWriter toServerPrintWriter= new PrintWriter(socket.getOutputStream());
+            toServerPrintWriter.println(httpRequest);
+            toServerPrintWriter.println(hostHeader);
+            toServerPrintWriter.println();
+            toServerPrintWriter.flush();
+
+            BufferedReader fromServerBufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String str;
+            while ((str = fromServerBufferedReader.readLine()) != null) {
+                System.out.println(str);
+            }
             System.out.println("Server up on port " + portNumber);
         } catch (IOException e) {
             System.err.println("Could not listen on port: " + portNumber + ".");
